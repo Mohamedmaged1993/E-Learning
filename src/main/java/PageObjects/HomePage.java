9976f201fb07e725a1df71eb40fff97901b426d9
Mohamed Maged <muhammed.maged1993@outlook.com>
2020-10-15 21:49:37 +0200

    add url configurations

--- src/main/java/PageObjects/HomePage.java
+++ src/main/java/PageObjects/HomePage.java
@@ -1,4 +1,15 @@
 package PageObjects;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class HomePage {
+    WebDriver driver;
+    WebDriverWait wait;
+
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(this.driver, 30);
+    }
 }
--- src/test/java/BaseTests/TestRuns.java
+++ src/test/java/BaseTests/TestRuns.java
@@ -1,4 +1,41 @@
 package BaseTests;
 
+import PageObjects.HomePage;
+import Utilities.ObjectRepo;
+import Utilities.PropertiesConfig;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
 public class TestRuns {
+
+
+    WebDriver driver;
+    WebDriverWait wait;
+    HomePage homePage;
+    PropertiesConfig propertiesConfig;
+    int timeOut = 10;
+
+    @BeforeClass
+    public void beforeclass() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, timeOut);
+        homePage = new HomePage(driver);
+        propertiesConfig = new PropertiesConfig();
+        driver.get(propertiesConfig.getProperty("navigateUrl"));
+    }
+
+
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
 }
